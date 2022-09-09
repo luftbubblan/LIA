633ccc86acb4896eb6537295fee4614a191b8caf
Oscar Blomberg <oscarblomberg@hotmail.com>
2022-09-09 14:02:41 +0200

    popup open and blur works, design as skiss

---
+++ index.js
@@ -0,0 +1,5 @@
+function logInBtnPress() {
+    document.getElementById("logInPopup").classList.remove("hidden");
+    document.getElementById("textbox").classList.add("hidden");
+    document.getElementById("blurDiv").classList.add("blur-filter");
+}
