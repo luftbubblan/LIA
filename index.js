f6cc5122c1a5ca2a903c145faf21f2ff1ae8062e
Oscar Blomberg <oscarblomberg@hotmail.com>
2022-09-10 12:08:16 +0200

    background blocker done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function logInBtnPress() {
-    document.getElementById("logInPopup").classList.remove("hidden");
+    document.getElementById("backgroundBlocker").classList.remove("hidden");
     document.getElementById("textbox").classList.add("hidden");
     document.getElementById("blurDiv").classList.add("blur-filter");
 }
