87bd93394ef3b905a00b069a71e626829ec6d032
Oscar Blomberg <oscarblomberg@hotmail.com>
2022-09-12 12:42:29 +0200

    all funktionalitet klar

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-function logInBtnPress() {
-    document.getElementById("backgroundBlocker").classList.remove("hidden");
-    document.getElementById("textbox").classList.add("hidden");
-    document.getElementById("blurDiv").classList.add("blur-filter");
+function togglePopup() {
+    document.getElementById("backgroundBlocker").classList.toggle("hidden");
+    document.getElementById("textbox").classList.toggle("hidden");
+    document.getElementById("blurDiv").classList.toggle("blur-filter");
+    document.getElementById("NameInput").value = "";
 }
+
+const names = [];
+function logIn(e) {
+    const name = capitalizeFirstLetter(e.children[0].value);
+    let names = localStorage.getItem("names");
+    if(names.includes(name + ",")) {
+        document.getElementById("logInDivMessage").innerHTML = "Välkommen tillbaka! Du är inloggad som " + name; 
+    } else {
+        document.getElementById("logInDivMessage").innerHTML = "Du är inloggad som " + name;
+        names += name + ",";
+        localStorage.setItem("names", names);
+    }
+    togglePopup();
+    document.getElementById("logOutBtn").classList.toggle("hidden");
+    document.getElementById("logInBtn").classList.toggle("hidden");
+    
+}
+
+function logOut() {
+    document.getElementById("logInDivMessage").innerHTML = "";
+    document.getElementById("logInBtn").classList.toggle("hidden");
+    document.getElementById("logOutBtn").classList.toggle("hidden");
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// ---UNCOMMENT TO RESET LOCAL STORAGE---
+// localStorage.setItem("names", "");
